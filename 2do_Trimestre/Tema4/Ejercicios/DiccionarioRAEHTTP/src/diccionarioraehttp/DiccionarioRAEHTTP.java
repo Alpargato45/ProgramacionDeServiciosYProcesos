508bbd5c4a2700604e82b077c8371af60da75f97
Alpargato45 <jorgedelcid2004@gmail.com>
2024-01-10 10:38:57 +0100

    Ejercicios

--- 2do_Trimestre/Tema4/Ejercicios/DiccionarioRAEHTTP/src/diccionarioraehttp/DiccionarioRAEHTTP.java
+++ 2do_Trimestre/Tema4/Ejercicios/DiccionarioRAEHTTP/src/diccionarioraehttp/DiccionarioRAEHTTP.java
@@ -20,7 +20,7 @@ public class DiccionarioRAEHTTP {
             URLConnection conexionURL = (HttpURLConnection) url.openConnection();
             conexionURL.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 OPR/77.0.4054.254");
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(conexionURL.getInputStream(), "UTF-8"));
-                 BufferedWriter writer = new BufferedWriter(new FileWriter("diccionario.html"))) {
+                 BufferedWriter writer = new BufferedWriter(new FileWriter("diccionario-" + palabra + ".html"))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
                     writer.write(line);
